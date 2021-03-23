908c6ec18a364746edf9b59f8ce30467cdcc0718
Kane <Kgin888@gmail.com>
2021-03-23 11:30:43 +0000

    separated exercises from directory

--- directort_ex.rb
+++ directort_ex.rb
@@ -23,12 +23,52 @@ def input_students
     puts "Enter cohort start month"
     cohort = gets.chomp.to_sym
     cohort = "N/A" if cohort.empty?
-    @students << { name: name, cohort: cohort }
+    puts "Enter #{name}'s country of birth"
+    country = gets.chomp
+    country = "N/A" if country.empty?
+    puts "Enter #{name}'s height"
+    height = gets.chomp
+    height = "N/A" if height.empty?
+    @students << { name: name, cohort: cohort, country: country, height: height }
     @students.count == 1 ? (puts "Now we have #{@students.count} student") : (puts "Now we have #{@students.count} students")
     puts "Enter next student's name"
     name = gets.chomp
   end
   @students
+end
+
+def print_letter(names, letter)
+  names.each_with_index { |name, ind| puts "#{ind + 1}. #{name[:name]} (#{name[:cohort]} cohort)".center(100) if name[:name][0].downcase == letter.downcase}
+end
+
+def print_length(names, length)
+  names.each_with_index { |name, ind| puts "#{ind + 1}. #{name[:name]} (#{name[:cohort]} cohort)".center(100) if name[:name].length < length}
+end
+
+def info(name, students)
+  students.each do |student|
+    if student[:name] == name
+      puts "#{student[:name]}:".center(100)
+      puts "Cohort: #{student[:cohort]}".center(100)  
+      puts "Height: #{student[:height]}".center(100)  
+      puts "Country of birth: #{student[:country]}".center(100)  
+    end
+  end
+end
+      
+  
+def each_to_while(names)
+  acc = 0
+  while acc < names.length
+    puts "#{acc + 1}. #{names[acc][:name]} (#{names[acc][:cohort]} cohort)"
+    acc += 1
+  end
+end
+
+def group_by_cohort(students)
+  students.group_by { |student| student[:cohort] }.each do |cohort, members|
+    members.each { |person| puts "#{person[:name]} (#{person[:cohort]} cohort)".center(100)}
+  end
 end
 
 def print_menu
@@ -63,6 +103,7 @@ def interactive_menu
   end
 end
 
+#can replace .chomp with [0..-2]
 
 interactive_menu
 
